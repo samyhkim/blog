ef16688f42b637d84f2d499193013787c35331d4
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-12-06 13:45:49 -0800

    Rendered list of posts and created fetchUser action creator.

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,11 +6,31 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderList() {
+    return this.props.posts.map(post => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
   render() {
-    return <div>Post List</div>;
+    console.log(this.props.posts);
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
-const mapStateToProps = state => {};
+// Any time we want to get data from Redux side of application to the React side
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
 
-export default connect(null, { fetchPosts })(PostList);
+export default connect(mapStateToProps, { fetchPosts })(PostList);
